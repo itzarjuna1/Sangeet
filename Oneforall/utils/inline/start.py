7a3e61d708af3c40ebe10528bbd1debf724777f2
UCHIHABOTS <txkuze@gmail.com>
2026-01-07 02:13:46 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -23,10 +23,10 @@ def private_panel(_):
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
-        ]
-         InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
