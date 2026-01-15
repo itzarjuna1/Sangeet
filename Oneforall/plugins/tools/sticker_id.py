64c2780ca917a5c206199e5879b9bbf455a40ac8
ARJUN <pachangedinanath@gmail.com>
2026-01-15 18:57:10 +0530

    Update sticker_id.py

--- Oneforall/plugins/tools/sticker_id.py
+++ Oneforall/plugins/tools/sticker_id.py
@@ -1,16 +1,32 @@
 from pyrogram import filters
 from Oneforall import app
 
-@app.on_message(filters.sticker)
-async def sticker_id(_, message):
-    sticker = message.sticker
+@app.on_message(filters.command("stickid") & filters.reply)
+async def stickid(_, message):
+
+    reply = message.reply_to_message
+
+    # Check if replied message has sticker
+    if not reply or not reply.sticker:
+        return await message.reply_text(
+            "❌ Reply to a sticker with `/stickid`",
+            parse_mode="markdown"
+        )
+
+    sticker = reply.sticker
 
     text = (
-        "🎯 **sᴛɪᴄᴋᴇʀ ɪᴅ ғᴏᴜɴᴅ!**\n\n"
-        f"📌 **ғɪʟᴇ ɪᴅ:**\n"
-        f"`{sticker.file_id}`\n\n"
-        f"📦 **ғɪʟᴇ ᴜɴɪǫᴜᴇ ɪᴅ:**\n"
-        f"`{sticker.file_unique_id}`"
+        "<blockquote>"
+        "<b>🎯 sᴛɪᴄᴋᴇʀ ɪᴅ ғᴏᴜɴᴅ!</b>\n\n"
+        "<b>📌 ғɪʟᴇ ɪᴅ:</b>\n"
+        f"<code>{sticker.file_id}</code>\n\n"
+        "<b>📦 ғɪʟᴇ ᴜɴɪǫᴜᴇ ɪᴅ:</b>\n"
+        f"<code>{sticker.file_unique_id}</code>"
+        "</blockquote>"
     )
 
-    await message.reply_text(text)
+    await message.reply_text(
+        text,
+        parse_mode="html",
+        disable_web_page_preview=True
+    )
