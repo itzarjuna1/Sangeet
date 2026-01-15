30f4765adac752e22b74c1513ec3492ebe574dfc
ARJUN <pachangedinanath@gmail.com>
2026-01-15 15:17:25 +0530

    Create sticker_id.py

---
+++ Oneforall/plugins/tools/sticker_id.py
@@ -0,0 +1,16 @@
+from pyrogram import filters
+from Tune import app
+
+@app.on_message(filters.sticker)
+async def sticker_id(_, message):
+    sticker = message.sticker
+
+    text = (
+        "🎯 **sᴛɪᴄᴋᴇʀ ɪᴅ ғᴏᴜɴᴅ!**\n\n"
+        f"📌 **ғɪʟᴇ ɪᴅ:**\n"
+        f"`{sticker.file_id}`\n\n"
+        f"📦 **ғɪʟᴇ ᴜɴɪǫᴜᴇ ɪᴅ:**\n"
+        f"`{sticker.file_unique_id}`"
+    )
+
+    await message.reply_text(text)
